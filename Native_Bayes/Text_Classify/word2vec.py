57f971785e3ceefd01a5621462c8ee7e17f5461f
Crisescode <zhaopanp2018@outlook.com>
2018-04-28 15:25:59 +0800

    ten commit

--- Native_Bayes/Text_Classify/word2vec.py
+++ Native_Bayes/Text_Classify/word2vec.py
@@ -9,3 +9,14 @@ vecList1 = bayes.setOfWords2Vec(myVocabList,listOPosts[0])
 vecList4 = bayes.setOfWords2Vec(myVocabList,listOPosts[3])
 print(vecList1)
 print(vecList4)
+
+trainMat = []
+for postinDoc in listOPosts:
+    trainMat.append(bayes.setOfWords2Vec(myVocabList,postinDoc))
+#print(trainMat)
+print(sum(listClasses))
+
+p0V,p1V,pAb = bayes.trainNB0(trainMat,listClasses)
+print(pAb)
+print(p0V)
+print(p1V)
